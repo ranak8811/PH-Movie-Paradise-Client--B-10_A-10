60fdb224466e195104235b8e05c5cda07c86cfd8
Anwar_Hossain <ranakrphone@gmail.com>
2024-12-05 18:17:45 +0600

    search functionality added in all movie page

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -4,15 +4,16 @@ import MovieCard from "../components/movieCard";
 
 const AllMovies = () => {
   const [allMovies, setAllMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:4000/allMovies")
+    fetch(`http://localhost:4000/allMovies?searchParams=${search}`)
       .then((res) => res.json())
       .then((data) => {
         setAllMovies(data);
       })
       .catch((error) => console.error("Error fetching movies:", error));
-  }, []);
+  }, [search]);
   console.log(allMovies);
   return (
     <div className="bg-gradient-to-r from-black via-gray-900 to-black p-4">
@@ -22,6 +23,16 @@ const AllMovies = () => {
           subtitle={"This is our collection for all movies"}
         ></Heading>
       </header>
+      <div className="max-w-[600px] mx-auto mb-4">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          name="search"
+          placeholder="Search movies using title..."
+          className="input input-bordered w-full"
+          required
+        />
+      </div>
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {allMovies.map((movie) => (
           <MovieCard key={movie._id} movie={movie}></MovieCard>
